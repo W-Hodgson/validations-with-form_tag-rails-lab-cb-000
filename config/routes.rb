fc8f04222a9979b1d5935f8930a0c8f14a064eb5
Will Hodgson <will.hodgson@current-rms.com>
2019-10-28 14:33:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :author, only: [:new, :create, :edit, :update, :show]
-  resources :post
+  resources :post, only: [:new, :create, :edit, :update, :show]
 end
